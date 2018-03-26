cf45bdeee9be4ee9992d3e9c1d3354f4a6281d38
Gerardo Castro <gerardoccf@gmail.com>
2018-03-26 18:44:52 -0300

    added check in_array multidimensional arrays and check in the bank if exists

--- functions/crawler/salvarEdicoes.php
+++ functions/crawler/salvarEdicoes.php
@@ -1,4 +1,13 @@
 <?php
+    function in_array_r($needle, $haystack, $strict = false) {
+        foreach ($haystack as $item) {
+            if (($strict ? $item === $needle : $item == $needle) || (is_array($item) && in_array_r($needle, $item, $strict))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     function salvarEdicoes() {
         $target_url = "https://www.mtggoldfish.com/prices/select";
         $html = new simple_html_dom();
@@ -7,13 +16,18 @@
         preg_match_all("/<a href='\/index\/([A-z,0-9]*)'.*\n<img.*>\n(.*)/", $html,$result);
 
         $insertinto = "INSERT INTO edicoes (nome, code) VALUES ";
+        $currentBank = DBRead('edicoes',null,'code');
 
         for ($i=0; $i < count($result[0]); $i++) { 
             $link = $result[1][$i];
             $nome = $result[2][$i];
-            if($i > 0) $insertinto = $insertinto.",";
-            $insertinto = $insertinto." ('$nome','$link')";
-            echo "Carta: {$nome} - Links: {$link}<br>";
+            if(in_array_r($link, $currentBank)){
+                echo "Edição: {$nome} - Já existe no banco!<br>";
+            } else {
+                if($i > 0) $insertinto = $insertinto.",";
+                $insertinto = $insertinto." ('$nome','$link')";
+                echo "Carta: {$nome} - Links: {$link}<br>";
+            }
         }
         DBExecute($insertinto);
     }
