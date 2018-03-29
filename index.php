3244d68a490f38f9d5862335db4440c2110ff1f1
Gerardo Castro <gerardoccf@gmail.com>
2018-03-29 18:05:18 -0300

    Added jquery and started configuring index.php

--- index.php
+++ index.php
@@ -1,25 +1,50 @@
-<?php
-/*
-require_once('inc/functions.php'); 
-require_once('inc/languages.php'); 
-require_once('Smarty.class.php');
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>MTGCrawler</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-$smarty = new Smarty;
-$current_lang = set_lang();
-$smarty->assign('lang',$lang[$current_lang]);
-$extensions = '';
-$loaded_ext = get_loaded_extensions(); foreach ($loaded_ext as $ext) $extensions.=$ext.', ';
-$smarty->assign('extensions',$extensions);
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">MTGCrawler</a>
+        <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#nav-header" aria-controls="nav-header"
+            aria-expanded="false" aria-label="Mudar navegação">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="nav-header">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdownId">
+                        <a class="dropdown-item" href="#">Action 1</a>
+                        <a class="dropdown-item" href="#">Action 2</a>
+                    </div>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
 
-if (((extension_loaded('mysqli')))&&(@mysqli_connect('localhost','root','vertrigo'))) 
- $smarty->assign('password_status', false); 
-else 
- $smarty->assign('password_status', true);
 
-$smarty->assign('php_version', phpversion());
- 
-$smarty->display('index.tpl');
-*/
-?>
 
-<h1>Bem vindo!</h1>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
